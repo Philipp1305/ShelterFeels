182a933a78711adb69917d63fc6e18feafd8808d
Anna Fey Winkler <annafey.winkler@akquinet.de>
2024-01-27 14:15:15 +0100

    windOW

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from gui.example import build_wind
+from gui.example import build_window
 from voice_recognition_app.inference_remote import extract_key_words_online
 
 
 def main():
     kw = extract_key_words_online()
-    build_wind(kw)
+    build_window(kw)
 
 
 if __name__ == "__main__":
