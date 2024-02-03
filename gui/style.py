af7f2a557b03e693982efa0139ef8415e2f5b928
Anna Fey Winkler <annafey.winkler@akquinet.de>
2024-02-03 18:02:50 +0100

    roboto

--- gui/style.py
+++ gui/style.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class Style:
     default_background = 'grey'
-    default_text_font = 'Comic Sans MS'
+    default_text_font = 'Robot Mono'
     default_text_color = 'black'
 
 style = Style()
